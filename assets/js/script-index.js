53dea1759d0dd9fec7a86ccdea386f451d7cb175
Lucas Simidu <lucas.simidu@outlook.com>
2024-09-21 02:59:46 -0300

    FEAT/ Adicionando conteúdo das páginas: Index, Loja, Stream, Forum.

---
+++ assets/js/script-index.js
@@ -0,0 +1,22 @@
+const headerContainer = document.getElementById('nav');
+
+fetch('assets/components/header.html')
+  .then(response => response.text())
+  .then(data => {
+    headerContainer.innerHTML = data;
+  })
+  .catch(error => {
+    console.error('Erro ao carregar a header:', error);
+  });
+
+const footerContainer = document.getElementById('footer');
+
+fetch('assets/components/footer.html')
+  .then(response => response.text())
+  .then(data => {
+    footerContainer.innerHTML = data;
+  })
+  .catch(error => {
+    console.error('Erro ao carregar o footer:', error);
+  });
+
