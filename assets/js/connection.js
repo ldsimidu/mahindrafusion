e43b1a6b9fddd9b985f23e43f305baf53fdadcb9
Lucas Simidu <lucas.simidu@outlook.com>
2024-11-04 11:30:17 -0300

    FEAT/ Inicio do cadastro e login de contas

---
+++ assets/js/connection.js
@@ -0,0 +1,53 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBanoiCrthH1-drvwHLDUM2s-mPBlEW3Yg",
+    authDomain: "indexeddb-6d93d.firebaseapp.com",
+    projectId: "indexeddb-6d93d",
+    storageBucket: "indexeddb-6d93d.firebasestorage.app",
+    messagingSenderId: "442440895445",
+    appId: "1:442440895445:web:7f06c3401adc601e874b5c",
+    measurementId: "G-Z39L3YVENP"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+function registerUser() {
+    const email = document.getElementById("registerEmail").value;
+    const password = document.getElementById("registerPassword").value;
+    const name = document.getElementById("registerName").value;
+
+    auth.createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            return db.collection("users").doc(user.uid).set({
+                email: email,
+                nome: name,
+            });
+        })
+        .then(() => {
+            alert("Cadastro realizado com sucesso!");
+            document.getElementById("registerForm").reset();
+        })
+        .catch((error) => {
+            console.error("Erro ao cadastrar usuário: ", error);
+            alert(error.message);
+        });
+}
+
+function loginUser() {
+    const email = document.getElementById("loginEmail").value;
+    const password = document.getElementById("loginPassword").value;
+
+    auth.signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            document.getElementById("welcomeMessage").textContent = "Bem-vindo, " + user.email + "!";
+            alert("Login realizado com sucesso!");
+            document.getElementById("loginForm").reset();
+        })
+        .catch((error) => {
+            console.error("Erro ao fazer login: ", error);
+            alert(error.message);
+        });
+}
